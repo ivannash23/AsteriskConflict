ed8cd647fb265953ab8f895ec0b9174469b17867
ivannash23 <ivannash23@Gmail.com>
2017-08-09 11:16:47 -0700

    Star class created

---
+++ main.js
@@ -0,0 +1,10 @@
+function Stars(){
+	this.fps = 30;
+	this.camvas = null;
+	this.height = 0;
+	this.width = 0;
+	this.minVelocity = 15;
+	this.maxVelocity = 30;
+	this.stars = 100;
+	this.intervalId = 0;
+}
