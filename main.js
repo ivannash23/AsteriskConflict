776ee40ad276f263c1e9244248c8ed422d2bdc32
ivannash23 <ivannash23@Gmail.com>
2017-08-09 11:43:38 -0700

    handle resize and create canvas

--- main.js
+++ main.js
@@ -8,3 +8,25 @@ function Stars(){
 	this.stars = 100;
 	this.intervalId = 0;
 }
+
+Stars.prototype.initialise = function(div) {
+
+	this.containerDiv = div;
+	this.width = window.innerWidth;
+	this.height = window.innerHeight;
+
+	window.addEventListener('resize', function resize(event){
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+		this.draw();
+	});
+
+	var canvas = document.createElement('canvas');
+	div.appendChild(canvas);
+	this.canvas = canvas;
+	this.canvas.width = this.width;
+	this.canvas.height = this.height;
+
+};
